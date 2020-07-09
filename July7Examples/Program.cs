69d415c9ba4f611970ee8682dcedc88f3a45f0bf
Justin <justin.zachow@gmail.com>
2020-07-08 21:36:09 -0400

    Commit July 8th exercises

--- July7Examples/Program.cs
+++ July7Examples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace July7Examples
 {
@@ -6,9 +7,41 @@ namespace July7Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Exercise 1
+             * 
+             * Console.WriteLine("Please enter a string!");
+             string input = Console.ReadLine();
+             Console.WriteLine($"You entered: {input}");*/
+
+
+
+            /* Exercise 2
+             * 
+             * Console.WriteLine("Please enter a number");
             var input = Console.ReadLine();
-            int x = int.Parse(input);
+            int x = int.Parse(input) + 1;
+            Console.WriteLine(x);
+
+            */
+
+            /* Exercise 3
+             * 
+            Console.WriteLine("Please enter a number");
+            double input = double.Parse(Console.ReadLine()) + 0.5;
+            Console.WriteLine(input);
+
+            */
+
+
+            Console.WriteLine("Please enter a number: \n");
+            double numOne = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number: \n");
+            double numTwo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The sum of your numbers is:\n" + (numOne + numTwo));
+
+
         }
     }
 }
