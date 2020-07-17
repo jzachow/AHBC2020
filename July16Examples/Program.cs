a5248ce0b77e209dbbe13a0e4665a10ccb9f3983
Justin <justin.zachow@gmail.com>
2020-07-16 21:49:18 -0400

    Commit July 16 examples

---
+++ July16Examples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace July16Examples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //StringBuilder
+            //var stringBuilder = new StringBuilder("Darion is teaching");
+            //stringBuilder.Append(" but we don't understand.");
+            //Console.WriteLine(stringBuilder);
+            //Console.WriteLine(stringBuilder.ToString());
+
+            //Exceptions
+            var userInput = Console.ReadLine();
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            finally
+            {
+                var hi = "hi";
+            }
+
+        }
+    }
+}
