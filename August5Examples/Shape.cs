60db40d79e3a469c4c3e23cff5050f29158df034
Justin <justin.zachow@gmail.com>
2020-08-05 19:43:10 -0400

    Commit Aug 5 exercise

---
+++ August5Examples/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace August5Examples
+{
+    abstract class Shape
+    {
+        public abstract int NumberOfSides { get; set; }
+
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();             
+       
+    }
+}
---
+++ August5Examples/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace August5Examples
+{
+    class Square : Shape
+    {
+        public override int NumberOfSides { get; set; }
+
+        public double SideLength { get; set; }
+
+        public override double CalculateArea()
+        {
+            return SideLength * SideLength;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return SideLength * 4;
+        }
+
+        public Square(double sideLength)
+        {
+            SideLength = sideLength;
+            NumberOfSides = 4;
+        }
+    }
+}
